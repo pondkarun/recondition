d79d6e578147cf33393cdfe67f45307f2a1bb0b3
stock <pondkarun@gmail.com>
2020-02-03 23:37:34 +0700

    device

--- service/appMenuService.js
+++ service/appMenuService.js
@@ -1,12 +1,12 @@
 ﻿'use strict'
 //demo
-app.config(function ($routeProvider, $mdDateLocaleProvider) {
+app.config(function($routeProvider, $mdDateLocaleProvider) {
 
-    $mdDateLocaleProvider.formatDate = function (date) {
+    $mdDateLocaleProvider.formatDate = function(date) {
         return date ? moment(date).format('DD-MM-YYYY') : '';
     };
 
-    $mdDateLocaleProvider.parseDate = function (dateString) {
+    $mdDateLocaleProvider.parseDate = function(dateString) {
         var m = moment(dateString, 'DD-MM-YYYY', true);
         return m.isValid() ? m.toDate() : new Date(NaN);
     };
@@ -15,7 +15,7 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
         templateUrl: "app/demoFormInput/template/input-form.html",
         controller: "appController",
         resolve: {
-            check: function ($location, userService) {
+            check: function($location, userService) {
                 if (!userService.isUserLoggedIn()) {
                     $location.path('/login');
                 }
@@ -25,7 +25,7 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
         templateUrl: "app/demoFormInput/template/input-form.html",
         controller: "appController",
         resolve: {
-            check: function ($location, userService) {
+            check: function($location, userService) {
                 if (!userService.isUserLoggedIn()) {
                     $location.path('/login');
                 }
@@ -35,7 +35,7 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
         templateUrl: "app/login/template/login.html",
         controller: "loginController",
         resolve: {
-            check: function ($location, userService) {
+            check: function($location, userService) {
                 if (userService.isUserLoggedIn()) {
                     $location.path("account");
                 }
@@ -45,7 +45,7 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
         templateUrl: "app/account/template/account.html",
         controller: "accountController",
         resolve: {
-            check: function ($location, userService, $http) {
+            check: function($location, userService, $http) {
                 if (!userService.isUserLoggedIn()) {
                     $location.path('/login');
                 } else {
@@ -65,7 +65,7 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
         templateUrl: "app/inventory/searchInventory/template/inventory.html",
         controller: "inventoryController",
         resolve: {
-            check: function ($location, userService, $http) {
+            check: function($location, userService, $http) {
                 if (!userService.isUserLoggedIn()) {
                     $location.path('/login');
                 } else {
@@ -85,7 +85,7 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
         templateUrl: "app/inventory/addEditInventory/template/addEditInventory.html",
         controller: "addEditInventoryController",
         resolve: {
-            check: function ($location, userService, $http) {
+            check: function($location, userService, $http) {
                 if (!userService.isUserLoggedIn()) {
                     $location.path('/login');
                 } else {
@@ -101,11 +101,51 @@ app.config(function ($routeProvider, $mdDateLocaleProvider) {
                 }
             },
         },
+    }).when("/device", {
+        templateUrl: "app/device/searchDevice/template/device.html",
+        controller: "deviceController",
+        resolve: {
+            check: function($location, userService, $http) {
+                if (!userService.isUserLoggedIn()) {
+                    $location.path('/login');
+                } else {
+                    let model = {
+                        ROUTEP: "device",
+                        ID_STATUS_EM: userService.getStatusID()
+                    }
+                    $http.post(webURL.webApi + "menu/chackMenuUserService.php", model).then((res) => {
+                        if (Number(res.data.COUNT_ID) <= 0) {
+                            $location.path('/account');
+                        }
+                    })
+                }
+            },
+        },
+    }).when("/device/:type/:ID", {
+        templateUrl: "app/device/addEditDevice/template/addEditDevice.html",
+        controller: "addEditDeviceController",
+        resolve: {
+            check: function($location, userService, $http) {
+                if (!userService.isUserLoggedIn()) {
+                    $location.path('/login');
+                } else {
+                    let model = {
+                        ROUTEP: "device",
+                        ID_STATUS_EM: userService.getStatusID()
+                    }
+                    $http.post(webURL.webApi + "menu/chackMenuUserService.php", model).then((res) => {
+                        if (Number(res.data.COUNT_ID) <= 0) {
+                            $location.path('/account');
+                        }
+                    })
+                }
+            },
+        },
     }).otherwise({ redirectTo: '/account' });
 });
 
 
 
-var checkPermission = function (authService) {
+var checkPermission = function(authService) {
     authService.checkPermission();
 }
